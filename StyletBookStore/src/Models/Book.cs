62b9625f77b18d4ec6203d238d44ce25609bf5af
wakuflair <130427427@qq.com>
2020-01-18 21:19:21 +0800

    创建Model: Book

---
+++ StyletBookStore/src/Models/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Stylet;
+
+namespace StyletBookStore.Models
+{
+    /// <summary>
+    /// 图书
+    /// </summary>
+    public class Book : PropertyChangedBase
+    {
+        public string Name { get; set; }
+
+        public BookType Type { get; set; }
+
+        public DateTime PublishDate { get; set; }
+
+        public float Price { get; set; }
+
+        public Book(string name, BookType type, DateTime publishDate, float price)
+        {
+            Name = name;
+            Type = type;
+            PublishDate = publishDate;
+            Price = price;
+        }
+    }
+}
---
+++ StyletBookStore/src/Models/BookType.cs
@@ -0,0 +1,30 @@
+﻿namespace StyletBookStore.Models
+{
+    public enum BookType
+    {
+        /// <summary>
+        /// 未定义
+        /// </summary>
+        Undefined,
+
+        /// <summary>
+        /// 传记
+        /// </summary>
+        Biography,
+
+        /// <summary>
+        /// 奇幻
+        /// </summary>
+        Fantastic,
+
+        /// <summary>
+        /// 恐怖
+        /// </summary>
+        Horror,
+
+        /// <summary>
+        /// 科幻
+        /// </summary>
+        ScienceFiction,
+    }
+}
