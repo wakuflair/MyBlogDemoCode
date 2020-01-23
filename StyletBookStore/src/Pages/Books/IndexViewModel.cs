7c1cfc2cf6c9b5a776065b595cf9dd62f5e7fd6c
wakuflair <130427427@qq.com>
2020-01-23 12:44:41 +0800

    图书列表界面

--- StyletBookStore/src/Pages/Books/IndexViewModel.cs
+++ StyletBookStore/src/Pages/Books/IndexViewModel.cs
@@ -8,16 +8,17 @@ namespace StyletBookStore.Pages.Books
     public class IndexViewModel : Screen
     {
         public ObservableCollection<Book> Books { get; set; }
+        public Book? SelectedBook { get; set; }
 
         public IndexViewModel()
         {
             Books = new ObservableCollection<Book>(new []
             {
-                new Book("阿米尔·汗：我行我素", BookType.Biography, DateTime.Parse("2017-6"), 52.8f, "9787559605115"), 
-                new Book("三体：“地球往事”三部曲之一", BookType.ScienceFiction, DateTime.Parse("2008-1"), 23f, "9787536692930"), 
-                new Book("三体Ⅱ：黑暗森林", BookType.ScienceFiction, DateTime.Parse("2008-5"), 32f, "9787536693968"), 
-                new Book("三体Ⅲ：死神永生", BookType.ScienceFiction, DateTime.Parse("2010-11"), 32f, "9787229030933"), 
-                new Book("肖申克的救赎", BookType.Mystery, DateTime.Parse("2006-7"), 26.9f, "9787020054985"), 
+                new Book("阿米尔·汗：我行我素", BookType.Biography, DateTime.Parse("2017-6"), 52.8f, "https://img1.doubanio.com/view/subject/l/public/s29467958.jpg"), 
+                new Book("三体：“地球往事”三部曲之一", BookType.ScienceFiction, DateTime.Parse("2008-1"), 23f, "https://img1.doubanio.com/view/subject/l/public/s2768378.jpg"), 
+                new Book("三体Ⅱ：黑暗森林", BookType.ScienceFiction, DateTime.Parse("2008-5"), 32f, "https://img3.doubanio.com/view/subject/l/public/s3078482.jpg"), 
+                new Book("三体Ⅲ：死神永生", BookType.ScienceFiction, DateTime.Parse("2010-11"), 32f, "https://img9.doubanio.com/view/subject/l/public/s26012674.jpg"), 
+                new Book("肖申克的救赎", BookType.Mystery, DateTime.Parse("2006-7"), 26.9f, "https://img9.doubanio.com/view/subject/l/public/s4007145.jpg"), 
             });
         }
     }
