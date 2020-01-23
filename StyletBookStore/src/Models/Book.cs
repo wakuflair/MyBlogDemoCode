735bd722d4cfd6817a3d8863c974fbced262e053
wakuflair <130427427@qq.com>
2020-01-23 12:44:23 +0800

    Book Model

--- StyletBookStore/src/Models/Book.cs
+++ StyletBookStore/src/Models/Book.cs
@@ -8,23 +8,38 @@ namespace StyletBookStore.Models
     /// </summary>
     public class Book : PropertyChangedBase
     {
+        /// <summary>
+        /// 书名
+        /// </summary>
         public string Name { get; set; }
 
+        /// <summary>
+        /// 类型
+        /// </summary>
         public BookType Type { get; set; }
 
+        /// <summary>
+        /// 出版年月
+        /// </summary>
         public DateTime PublishDate { get; set; }
 
+        /// <summary>
+        /// 价格
+        /// </summary>
         public float Price { get; set; }
 
-        public string Isbn { get; set; }
+        /// <summary>
+        /// 封面URL
+        /// </summary>
+        public string CoverUrl { get; set; }
 
-        public Book(string name, BookType type, DateTime publishDate, float price, string isbn)
+        public Book(string name, BookType type, DateTime publishDate, float price, string coverUrl)
         {
             Name = name;
             Type = type;
             PublishDate = publishDate;
             Price = price;
-            Isbn = isbn;
+            CoverUrl = coverUrl;
         }
     }
 }
